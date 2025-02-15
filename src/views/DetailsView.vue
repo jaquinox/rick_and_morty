a699d132ab6ca34b64ea3c441405044ba6d6d55e
aquino <jaquinox123@gmail.com>
2025-02-15 18:35:53 -0400

    use router y details

---
+++ src/views/DetailsView.vue
@@ -0,0 +1,77 @@
+
+<template>
+
+<main>
+
+    <!--
+    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-center gap-4 mt-24 mx-10" >
+      <div  class="rounded-2xl  overflow-hidden  shadow-2xl "  >
+          <div >
+            <img :src="character.image" :alt="character.name"  class="" />
+                  <div class="text-center">
+                    {{ character.name }}
+                  </div>                
+          </div>    
+      </div>
+    </div>
+
+-->
+
+
+    <div v-if="character">
+
+        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-center gap-4 mt-24 mx-10" >
+        <div  class="rounded-2xl  overflow-hidden  shadow-2xl "  >
+            <div >
+                <img :src="character.image" :alt="character.name"  class="" />
+                    <div class="text-center col-auto">
+                        {{ character.name }} <br>
+                        {{ character.status }} <br>
+                        {{ character.species }} <br>
+                        {{ character.gender }} <br>
+                        {{ character.origin.name }} <br>
+                        {{ character.location.name }} <br>
+                       
+                    </div>                
+            </div>    
+        </div>
+        </div>
+
+
+    </div>
+
+
+</main>
+
+
+</template>
+
+<script setup lang="ts">
+
+import{ref, onMounted} from 'vue'
+
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const id = route.params.id;
+const character  = ref();
+
+
+const loadCharacter = async () => {
+      const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+      const data = await response.json()
+      character.value = data
+
+      console.log(character.value)
+    }
+
+
+
+    onMounted(() => {
+      loadCharacter()
+    })
+
+
+
+</script>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,33 +4,71 @@
       <h1 class="text-5xl text-red-500">Rick y Morty</h1>
     </div>
 
-    <div class="grid grid-cols-6 justify-center gap-4 mt-24 mx-10" >
-      <div v-for="character in characters" :key="characters.id"  class="rounded-2xl  overflow-hidden  shadow-2xl "  >
-        <img :src="character.image" :alt="character.name"  class="" />
-        <div class="text-center">
-          {{ character.name }}
-        </div>
-        
+    <div class="flex justify-center mt-5">
+
+      <button @click="decrementPage" class="me-5 rounded-md bg-orange-800 px-10.5 py-2.5 text-sm size-30 text-white shadow-xs" ><spam class="text-5xl font-extrabold">-</spam></button>
+
+      <p class="text-5xl font-extrabold"> {{ page }}</p>
+
+      <button @click="incrementPage" class="ms-5 rounded-md bg-indigo-600 px-10.5 py-2.5 text-sm  size-30 text-white shadow-xs" ><spam class="text-5xl font-extrabold">+</spam></button>
+
+    </div>
+
+
+
+    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-center gap-4 mt-24 mx-10" >
+      <div  v-for="character in characters" :key="characters.id"  class="rounded-2xl  overflow-hidden  shadow-2xl "  >
+          <div @click="seeCharacterDetails(character.id)">
+            <img :src="character.image" :alt="character.name"  class="" />
+                  <div class="text-center">
+                    {{ character.name }}
+                  </div>                
+          </div>    
       </div>
     </div>
   </main>
+
+
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
 
-const characters = ref([])
+    import { onMounted, ref } from 'vue'
+    import { useRouter } from 'vue-router'
 
-const loadCharacters = async () => {
-  const response = await fetch('https://rickandmortyapi.com/api/character')
-  const data = await response.json()
+    const router = useRouter()
+    const characters = ref([])
+    const page:any = ref(1)
 
-  characters.value = data.results
+    const loadCharacters = async () => {
+      const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page.value}`)
+      const data = await response.json()
+      characters.value = data.results
+      //console.log(characters.value)
+    }
 
-  console.log(characters.value)
-}
+    onMounted(() => {
+      loadCharacters()
+    })
 
-onMounted(() => {
-  loadCharacters()
-})
+    const incrementPage = () => {
+      page.value++
+      loadCharacters()
+    }
+
+    const seeCharacterDetails = (character_id:number) => {
+      router.push(`details/${character_id}`)
+    }
+
+    const decrementPage = () => {
+      if(page.value > 1){
+        page.value--
+        loadCharacters()  
+      }else{
+        page.value = 1
+        loadCharacters()
+      }
+    }
+
+
 </script>
